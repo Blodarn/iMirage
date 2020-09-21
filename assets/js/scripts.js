75957bfb4f84688edbec798fe154836e0fe4c943
Blodarn <blodarn@gmail.com>
2020-09-20 21:10:06 -0400

    Link update again

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -29,9 +29,9 @@ jQuery(document).ready(function() {
     /*
         Background slideshow
     */
-    $('.top-content').backstretch("https://blodarn.github.io/imirage/assets/img/backgrounds/background-chantier.jpg");
-    $('.call-to-action-container').backstretch("https://blodarn.github.io/imirage/assets/img/backgrounds/background-maison.jpg");
-    $('.testimonials-container').backstretch("https://blodarn.github.io/imirage/assets/img/backgrounds/background-maison.jpg");
+    $('.top-content').backstretch("https://blodarn.github.io/imirage/assets/img/backgrounds/Background-Chantier.jpg");
+    $('.call-to-action-container').backstretch("https://blodarn.github.io/imirage/assets/img/backgrounds/Background-Maison.jpg");
+    $('.testimonials-container').backstretch("https://blodarn.github.io/imirage/assets/img/backgrounds/Background-Maison.jpg");
     
     $('#top-navbar-1').on('shown.bs.collapse', function(){
     	$('.top-content').backstretch("resize");
